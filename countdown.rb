4aa01d17ff98b38636e14973fd2c683c468a9f24
Yonas Fesehatsion <yone920@gmail.com>
2019-04-09 14:35:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,11 +4,17 @@ def countdown(int)
   while int > 0
     puts "#{int} SECOND(S)!"
     int -= 1 
-    sleep 1
+    
   end
   "HAPPY NEW YEAR!"
 end
  
  
  countdown_with_sleep(int)
- 
+ while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1 
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
+end
